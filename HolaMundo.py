b3ec50636ef0df05acee36a065cdcd9ebee141b6
RodolfoGranados <116964133+RodolfoGranados@users.noreply.github.com>
2022-10-29 11:19:36 -0500

    1er Archivo

---
+++ HolaMundo.py
@@ -0,0 +1,11 @@
+# EJEMPLO 1. SALUDO
+# COMENTARIOS: sirven para documentar o detallar cada parte del código
+# A los comentarios no los toma en cuenta Python
+
+# Imprimiendo resultados.
+print("Hola Mundo")
+print('Rodolfo');
+
+print(10+5)
+
+
